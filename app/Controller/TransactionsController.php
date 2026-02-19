b4ee16ffd1b1dc48b837998e433817c19bae72c1
GiriPurushotam <giripurushotam39@gmail.com>
2026-02-19 22:33:49 +0545

    Add store method of create transaction & redirect auth user

--- app/Controller/TransactionsController.php
+++ app/Controller/TransactionsController.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Contracts\RequestValidatorFactoryInterface;
+use App\DataObjects\RegisterTransactionData;
 use App\Http\Response;
 use App\Http\ResponseInterface;
 use App\Http\ServerRequestInterface;
 use App\RequestValidators\CreateTransactionRequestValidator;
 use App\Services\CategoryService;
+use App\Services\TransactionService;
 use App\ViewRenderer;
 
 class TransactionsController
@@ -17,7 +19,8 @@ class TransactionsController
     public function __construct(
         private readonly ViewRenderer $view,
         private readonly CategoryService $categoryService,
-        private readonly RequestValidatorFactoryInterface $requestValidatorFactory
+        private readonly RequestValidatorFactoryInterface $requestValidatorFactory,
+        private readonly TransactionService $transactionService
     ) {}
 
     public function index(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
@@ -35,6 +38,17 @@ class TransactionsController
     public function store(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
         $data = $this->requestValidatorFactory->make(CreateTransactionRequestValidator::class)->validate($request->getParsedBody());
-        return $response->withHeader('Location', BASE_PATH . '/transaction')->withStatus(302);
+
+        $user = $request->getAttribute('user');
+
+        $transactionData = new RegisterTransactionData(
+            categoryId: (int) $data['category_id'],
+            description: $data['description'],
+            date: new \DateTimeImmutable($data['date']),
+            amount: (float) $data['amount']
+        );
+
+        $this->transactionService->create($transactionData, $user);
+        return $response->withHeader('Location', BASE_PATH . '/transactions')->withStatus(302);
     }
 }
