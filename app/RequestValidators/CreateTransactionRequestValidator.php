fbe64fdf7cdc85b7a2f6c33e37fc89fcc904e9e4
GiriPurushotam <giripurushotam39@gmail.com>
2026-02-19 15:42:48 +0545

    Create requestValidatorClass for to store transaction

---
+++ app/RequestValidators/CreateTransactionRequestValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\RequestValidators;
+
+use App\Contracts\RequestValidatorInterface;
+use App\Exceptions\ValidationException;
+use App\Validation\Validators;
+
+class CreateTransactionRequestValidator implements RequestValidatorInterface
+{
+    public function validate(array $data): array
+    {
+        $v = new Validators($data);
+
+        $v->rule('required', ['category_id', 'description', 'date', 'amount']);
+        $v->rule('integer', 'category_id');
+        $v->rule('min', 'category_id', 1);
+        $v->rule('lengthMax', 'description', 255);
+        $v->rule('dateFormat', 'date', 'Y-m-d');
+        $v->rule('numeric', 'amount');
+        $v->rule('min', 'amount', 0.01);
+
+        if (! $v->validate()) {
+            throw new ValidationException($v->errors());
+        }
+
+        return $data;
+    }
+}
